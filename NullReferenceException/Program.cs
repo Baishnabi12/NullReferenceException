c9d28d7428b4759be8157e7fe36364fc8680822e
baish <baish@BAISHNABI>
2024-10-17 13:31:33 +0530

    Add project files.

---
+++ NullReferenceException/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        try
+        {
+            string[] name= null;
+            Console.WriteLine(name[0]); 
+        }
+        catch (NullReferenceException ex)
+        {
+            Console.WriteLine(" The array is not initialized.");
+        }
+    }
+}
+
